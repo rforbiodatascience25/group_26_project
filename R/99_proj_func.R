941df19a375fc2462c7b35b0edc3fe71d866be2c
FelipeMoralesG <s252623@dtu.dk>
2025-11-25 14:14:36 +0100

    Add a function to 99_proj_func to save data. Fill 02_clean.qmd file with the data cleaning of most cleaning (copy-pasting and fixing the code from my classmates here, also adding mine). And uploading TB_household_estimates done by Dhruva so anyone can see it.

--- R/99_proj_func.R
+++ R/99_proj_func.R
@@ -3,20 +3,20 @@
 # -------- function to load raw data --------------------------
 
 raw_load_or_download <- function(data_file, data_url) {
-  
+
   # Ensure Data folder exists
   data_folder <- file.path("..", "_raw")
   if (!dir.exists(data_folder)) {
     message("Creating ../_raw folder…")
     dir.create(data_folder, recursive = TRUE)
   }
-  
+
   # Ensure data_file has full path
   if (!startsWith(data_file, file.path("..", "_raw"))) {
     data_file <- file.path(file.path("..", "_raw"), data_file)
   }
-  
-  # Load from disk OR download & save it through the link above: 
+
+  # Load from disk OR download & save it through the link above:
   if (file.exists(data_file)) {
     message(paste0("Loading ", data_file, " from local file…"))
     df <- read_csv(
@@ -38,6 +38,29 @@ raw_load_or_download <- function(data_file, data_url) {
   df
 }
 
-
-
 # ----------------------------------------------------------------------
+
+
+# -------- function to saw data --------------------------
+
+save_data <- function(df, data_name) {
+
+  # Ensure Data folder exists
+  data_folder <- file.path("..", "data")
+  if (!dir.exists(data_folder)) {
+    message("Creating ../data folder…")
+    dir.create(data_folder, recursive = TRUE)
+  }
+
+  # Ensure data_file has full path
+  if (!startsWith(data_name, file.path("..", "data"))) {
+    data_name <- file.path(file.path("..", "data"), data_name)
+  }
+
+  #Save it through to the /data folder:
+  message(paste0("Saving ", data_name, " to /data local file…"))
+  write_csv(
+    x = df,
+    file = data_name
+  )
+}
